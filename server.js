e08f340c711bbab8aa131e666024607ab05f9e0f
Keiti Nurmbek <keiti.nurmbe@gmail.com>
2021-09-23 11:08:09 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get("/", function (_, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+app.post('/', function (req, res) {
+    console.log(req.body);
+    let weight = Number(req.body.weight);
+    let height = Number(req.body.height * 0.01);
+    let BMI = round((weight / (height * height)), 1);
+
+    let answer
+    if (BMI<19) answer = (`BMI ${BMI}: You are underweight`);
+    else if (BMI<25 ) answer = (`BMI ${BMI}: You are in a healthy weight`);
+    else if (BMI < 30) answer = (`BMI ${BMI}: You are obese`);
+    else answer = (`BMI ${BMI}: You are severely obese`);
+
+    res.send(`${answer}`);
+});
+
+
+app.listen(3001, function () {
+    console.log("Server is running on port 3001.")
+})
+
+//to round to n decimal places
+function round(num, places) {
+    var multiplier = Math.pow(10, places);
+    return Math.round(num * multiplier) / multiplier;
+}
