f882b7c6c5e7a1054c952efaf5bda0a9be7d2a06
Keiti Nurmbek <keiti.nurmbe@gmail.com>
2021-09-23 11:10:27 +0300

    Changed port nr

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.post('/', function (req, res) {
 });
 
 
-app.listen(3001, function () {
-    console.log("Server is running on port 3001.")
+app.listen(3000, function () {
+    console.log("Server is running on port 3000.")
 })
 
 //to round to n decimal places
